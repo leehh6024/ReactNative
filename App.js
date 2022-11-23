91c2cfeb8182a91b4963fc5911fe6c0c280b8021
leehh6024 <dlgusghchlrh@gmail.com>
2022-11-24 03:35:07 +0900

    Study: ReactNative

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Seoul</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
+      <ScrollView
+        pagingEnabled
+        showsHorizontalScrollIndicator={false} // 페이지 하단 스크롤 상태표시 삭제하게 해준다.
+        // indicatorStyle="white" // 페이지 하단 스크롤 상태표시 색상 입히는것.
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -14,5 +40,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 168,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
